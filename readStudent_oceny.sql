7eb86688b7a1cbd830de512e354abe84484d657d
OlehTrukhan <oleg.truhan789@gmail.com>
2017-05-17 23:40:54 +0200

    Add files via upload

--- readStudent_oceny.sql
+++ readStudent_oceny.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE '~/Documents/db_project/student_oceny.txt' -- wczytuje plik txt z dannymi u mnie onie sa w takiej sciezce
+LOAD DATA LOCAL INFILE '~/Documents/db_project/student_oceny.txt'
 INTO TABLE Oceny FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
