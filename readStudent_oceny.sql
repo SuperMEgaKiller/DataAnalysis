7cfbb2885ee9a40468cb84496304e37828524f3e
OlehTrukhan <oleg.truhan789@gmail.com>
2017-05-17 23:39:32 +0200

    Update readStudent_oceny.sql

--- readStudent_oceny.sql
+++ readStudent_oceny.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE '~/Documents/db_project/student_oceny.txt'
+LOAD DATA LOCAL INFILE '~/Documents/db_project/student_oceny.txt' -- wczytuje plik txt z dannymi u mnie onie sa w takiej sciezce
 INTO TABLE Oceny FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
