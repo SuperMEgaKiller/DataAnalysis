d8383d5fdfd3b763716e4b02067b1a6b5c3a26aa
OlehTrukhan <oleg.truhan789@gmail.com>
2017-05-20 20:29:36 +0200

    Delete readStudent_obecnosci.sql

--- readStudent_obecnosci.sql
+++
@@ -1,4 +0,0 @@
-LOAD DATA LOCAL INFILE '~/Documents/db_project/obecnosci_data.txt' -- wczytuje plik txt z dannymi 
-INTO TABLE Obecnosc FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
