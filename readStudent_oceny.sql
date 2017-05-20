14d4a8aba620a282ad4f5b2b4cb9348de8e6c894
OlehTrukhan <oleg.truhan789@gmail.com>
2017-05-20 20:29:48 +0200

    Delete readStudent_oceny.sql

--- readStudent_oceny.sql
+++
@@ -1,4 +0,0 @@
-LOAD DATA LOCAL INFILE '~/Documents/db_project/student_oceny.txt' -- wczytuje plik txt z dannymi 
-INTO TABLE Oceny FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
