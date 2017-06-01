f979b4032d8f3222c5b326e77d8679f31ff62105
OlehTrukhan <oleg.truhan789@gmail.com>
2017-06-01 19:27:50 +0200

    Delete UpdateIloscObecnosci.sql

--- UpdateIloscObecnosci.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Ilosc_obecnosci (id, nr_obecnsci)  -- Obliczanie ilosci
-SELECT student_id, COUNT(data) AS nr_obecnosci -- obecnosci dla kazdego
-FROM Obecnosc GROUP BY student_id;             -- studenta
-
-INSERT INTO Ilosc_obecnosci (id)         -- Dodanie wszystkich studentow
-SELECT id FROM Student WHERE Student.id  -- bez obecnosci uswienie
-NOT IN(SELECT student_id FROM Obecnosc); -- obecnosci na 0
